4c8df7b30b222dbf58520d9a6dcb9cb57b9a9b73
Josep Valls <alfredvays@gmail.com>
2022-12-31 09:08:37 +0100

    Refactor withData

--- src/withData.js
+++ src/withData.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 
-export default function withData({ url, initialState }) {
-  return function (WrappedComponent) {
-    const WithDataComponent = ({ ...props }) => {
+export default function withData({ url, initialState }) { //Primero pasamos los parametros y devolvemos otra funcion;
+  return function (WrappedComponent) { //Funcion HOC que devyelve un componente mejorado
+    const WithDataComponent = ({ ...props }) => {// Crea el componente mejorado
       const [data, setData] = useState(initialState);
 
       useEffect(() => {
@@ -11,13 +11,13 @@ export default function withData({ url, initialState }) {
           .then((result) => setData(result.data));
       }, []);
 
-      return <WrappedComponent {...props} data={data} />;
+      return <WrappedComponent {...props} data={data} />; // pasamos todas las propos que recibe el componente a mejorar y le añadimos las mejoras(data)
     };
     WithDataComponent.displayName = `withData(${
         WrappedComponent.displayName ?? WrappedComponent.name
-      })`;
+      })`; // Seteamos el nombre del componente original al componente mejorado
     
       return WithDataComponent;
   };
   
-}
+};
